be203f269b1e40fc0ddebfc6a7e34291548252bf
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2022-09-06 03:23:46 -0400

    update index

--- server/index.js
+++ server/index.js
@@ -5,12 +5,7 @@ const seed = require('../script/seed');
 
 const init = async () => {
   try {
-    if(process.env.SEED === 'true'){
-      await seed();
-    }
-    else {
-      await db.sync()
-    }
+    await seed()
     // start listening (and create a 'server' object representing our server)
     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
   } catch (ex) {
@@ -19,3 +14,9 @@ const init = async () => {
 }
 
 init()
+// if(process.env.SEED === 'true'){
+
+// }
+// else {
+//   await db.sync()
+// }
